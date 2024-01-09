65feb50aee1cd79fca966785b2655169f90501ce
Akira <vlad30092004@gmail.com>
2024-01-10 01:18:04 +0200

    Add Auth

---
+++ AniTalkApi/Controllers/AuthController.cs
@@ -0,0 +1,182 @@
+﻿using AniTalkApi.DataLayer.DTO.Auth;
+using AniTalkApi.DataLayer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using AniTalkApi.DataLayer.Models.Enums;
+
+namespace AniTalkApi.Controllers;
+[Route("/[controller]")]
+
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<User> _userManager;
+    private readonly IConfiguration _configuration;
+
+    public AuthController(
+        UserManager<User> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginForm formData)
+    {
+
+        var user = formData.Login.Contains('@')
+            ? await _userManager.FindByEmailAsync(formData.Login)
+            : await _userManager.FindByNameAsync(formData.Login);
+
+        if (user == null || !await _userManager.CheckPasswordAsync(user, formData.Password)) 
+            return Unauthorized();
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        var authClaims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.UserName),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+        authClaims
+            .AddRange(userRoles
+                .Select(userRole => new Claim(ClaimTypes.Role, userRole)));
+
+        var token = CreateToken(authClaims);
+        var refreshToken = GenerateRefreshToken();
+
+        _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInDays"], out int refreshTokenValidityInDays);
+
+        user.RefreshToken = refreshToken;
+        user.RefreshTokenExpiryTime = DateTime.Now.AddDays(refreshTokenValidityInDays).ToUniversalTime();
+
+        await _userManager.UpdateAsync(user);
+
+        return Ok(new
+        {
+            Token = new JwtSecurityTokenHandler().WriteToken(token),
+            RefreshToken = refreshToken,
+            Expiration = token.ValidTo
+        });
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterForm formData)
+    {
+        if (await _userManager.FindByEmailAsync(formData.Email) is not null)
+            return StatusCode(StatusCodes.Status409Conflict, "The user with this email is already exist");
+
+        if (await _userManager.FindByNameAsync(formData.Username) is not null)
+            return StatusCode(StatusCodes.Status409Conflict, "The user with this name is already exist");
+
+        User user = new()
+        {
+            Email = formData.Email,
+            UserName = formData.Username,
+            DateOfRegistration = DateTime.Now,
+            SecurityStamp = Guid.NewGuid().ToString(),
+            Status = UserStatus.Online,
+            PersonalInformation = new PersonalInformation()
+        };
+        var result = await _userManager.CreateAsync(user, formData.Password);
+        if (!result.Succeeded)
+            return StatusCode(StatusCodes.Status500InternalServerError, 
+                "User creation failed! Please check user details and try again." );
+
+        return Ok("User created successfully!");
+    }
+
+
+    [HttpPost]
+    [Route("refresh-token")]
+    public async Task<IActionResult> RefreshToken(TokenModel? tokenModel)
+    { 
+        if (tokenModel is null)
+            return BadRequest("Invalid client request");
+
+        var accessToken = tokenModel.AccessToken;
+        var refreshToken = tokenModel.RefreshToken;
+
+        var principal = GetPrincipalFromExpiredToken(accessToken);
+        if (principal == null)
+            return BadRequest("Invalid access token or refresh token");
+
+        var username = principal.Identity.Name;
+
+        var user = await _userManager.FindByNameAsync(username);
+
+        if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+            return BadRequest("Invalid access token or refresh token");
+
+        var newAccessToken = CreateToken(principal.Claims.ToList());
+        var newRefreshToken = GenerateRefreshToken();
+
+        user.RefreshToken = newRefreshToken;
+        await _userManager.UpdateAsync(user);
+
+        return new ObjectResult(new
+        {
+            accessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+            refreshToken = newRefreshToken
+        });
+    }
+
+
+    private JwtSecurityToken CreateToken(IEnumerable<Claim> authClaims)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+        _ = int.TryParse(_configuration["JWT:TokenValidityInMinutes"], out var tokenValidityInMinutes);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidAudience"],
+            expires: DateTime.Now.AddMinutes(tokenValidityInMinutes),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+            );
+
+        return token;
+    }
+
+    private static string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[64];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    private ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"])),
+            ValidateLifetime = false
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+        if (securityToken is not JwtSecurityToken jwtSecurityToken || 
+            !jwtSecurityToken.Header.Alg
+                .Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+            throw new SecurityTokenException("Invalid token");
+
+        return principal;
+
+    }
+}
---
+++ AniTalkApi/DataLayer/DTO/Auth/LoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AniTalkApi.DataLayer.DTO.Auth;
+
+public class LoginForm
+{
+    [Required]
+    public string Login { get; init; }
+
+    [Required]
+    public string Password { get; init; }
+}
